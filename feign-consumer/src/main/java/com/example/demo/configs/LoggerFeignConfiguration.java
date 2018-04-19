103109087389facac5c7732a7d2ec3d06b7ab528
geney <guoit5@163.com>
2018-04-19 19:18:54 +0800

    提交logger打印对应的请求响应信息

--- feign-consumer/src/main/java/com/example/demo/configs/LoggerFeignConfiguration.java
+++ feign-consumer/src/main/java/com/example/demo/configs/LoggerFeignConfiguration.java
@@ -14,6 +14,7 @@ public class LoggerFeignConfiguration {
 
     @Bean
     Logger.Level loggerFeignLevel() {
+        //NONE 不记录任何信息 BASIC:记录请求方法URL以及响应状态码和执行时间 HEADERS basic+请求头信息  FULL:记录所有的请求响应明细
         return Logger.Level.FULL;
     }
 
